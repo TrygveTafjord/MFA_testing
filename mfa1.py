df4743cfa5c3d6f02685681f86f6c278af386b12
Trygve Myrland Tafjord <trygvemt@ntnu.no>
2026-02-11 15:13:17 +0100

    some small changes to the MFA-code, it needs more work!

--- mfa1.py
+++ mfa1.py
@@ -1,11 +1,13 @@
 import torch
 
+#Implementation is based on: https://www.cs.toronto.edu/~fritz/absps/tr-96-1.pdf
+
 class MFA(torch.nn.Module):
     def __init__(self, m_max, n_channels, k_max, tol=1e-4, max_iter=100, device='cpu'):
         super().__init__()
 
         # x = lambda * z + u
-        self.register_buffer('m', torch.empty(m_max)) # number of components (FA)
+        self.register_buffer('m', torch.empty(1)) # number of components (FA)
         self.register_buffer('k', torch.empty(m_max, k_max)) # number of factors
         self.register_buffer('Psi', torch.empty(n_channels)) # noise variances, the diagonal of the covariance matrix, we assume the noise to be the same across all components (sensor noise)
         self.register_buffer('lambda', torch.empty(n_channels, k_max)) # factor loadings
@@ -20,3 +22,13 @@ class MFA(torch.nn.Module):
     @property
     def device(self):
         return self.device_tracker.device
+    
+    def _e_step(self, X):
+        # Compute hij , E[zjxi; !j ] and E[zz0jxi; !j ] for all data points i and mixture components j.
+        
+        pass
+
+    def _m_step(self, X, responsibilities):
+        # M-step: Update parameters based on responsibilities
+        # This is where we update the parameters (means, factor loadings, noise variances, and mixing proportions) based on the responsibilities computed in the E-step.
+        pass
